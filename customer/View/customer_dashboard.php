828ecb358ad645f1feb91a56e1a4b04f2f7b67d5
Rifat <hossen.mdrifat20@gmail.com>
2022-06-11 19:14:01 +0600

    customer dashboard done

--- customer/View/customer_dashboard.php
+++ customer/View/customer_dashboard.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>customer dashboard</title>
 </head>
 <body>
-    <p>Welcome there!</p>
+    <header>
+        <div class="left_area">
+        <h3><span>Online </span><span>Laundry </span><span>Service</span></h3>
+        </div>
+        <div class="right_area">
+         
+        </div>
+    </header>
+   
+
+    <div class="sidebar">
+        <center>
+        <img src="../images/Best_Laundry.jpg" alt="dashboard banner"width="600">
+        </center>
+        <a href="customer_dashboard.php">Dashboard</a><br>
+        <a href="customer_profile.php">Profile</a><br> 
+        <a href="customer_dashboard.php">Order History</a><br> 
+        <a href="customer_dashboard.php">Contact Us</a><br>
+        <a href="login_page.php" class="logout_btn">Logout</a>
+    </div>
+
 </body>
 </html>
